f20f9e7fea9c7dfc93c0105bcd0173c766988522
Franco Marlon Ugarte Schulze <95712345+Francoschulze@users.noreply.github.com>
2022-07-26 13:57:12 -0300

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+# pages/urls.py
+from django.urls import path
+from .views import HomePageView, AboutPageView  # New
+
+urlpatterns = [
+    path('about/', AboutPageView.as_view(), name='about'),  # New
+    path('', HomePageView.as_view(), name='home'),
+]
